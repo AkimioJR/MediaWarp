c4001f8442c2cba1d83fde8d5700f2241162302d
AkimioJR <akimio.jr@gmail.com>
2025-10-25 15:09:35 +0800

    refacot: 调整中间件存放位置

--- internal/router/middleware.go
+++ internal/router/middleware.go
@@ -1,36 +1,10 @@
 package router
 
 import (
-	"net/url"
-	"strings"
-
 	"github.com/gin-gonic/gin"
 )
 
 type MiddlewareFunc func(internalFunc gin.HandlerFunc) gin.HandlerFunc
-
-func QueryKeyCaseInsensitive(internalFunc gin.HandlerFunc) gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		queryParams := make(url.Values)
-		for key, values := range ctx.Request.URL.Query() {
-			queryParams.Add(strings.ToLower(key), strings.Join(values, ","))
-		}
-		ctx.Request.URL.RawQuery = queryParams.Encode()
-
-		internalFunc(ctx)
-	}
-}
-
-var _ MiddlewareFunc = QueryKeyCaseInsensitive
-
-func DisableCompression(internalFunc gin.HandlerFunc) gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		ctx.Request.Header.Del("Accept-Encoding")
-		internalFunc(ctx)
-	}
-}
-
-var _ MiddlewareFunc = DisableCompression
 
 // MiddlewareChain 中间件链构建器
 type MiddlewareChain struct {
