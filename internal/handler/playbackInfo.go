d4d06d44096bb2de9b5821ef96b5476aae76c49e
AkimioJR <akimio.jr@gmail.com>
2026-02-09 12:47:52 +0800

    perfect: 优化 PlaybackInfo 直链处理行为和日志输出

--- internal/handler/playbackInfo.go
+++ internal/handler/playbackInfo.go
@@ -18,6 +18,13 @@ func processHTTPStrmPlaybackInfo(jsonChain *utils.JsonChain, bsePath string, ite
 		logging.Debugf("处理 HTTPStrm %s PlaybackInfo 耗时：%s", id, time.Since(startTime))
 	}()
 
+	var msgs []string
+
+	jsonChain.Set(
+		bsePath+"SupportsDirectPlay",
+		true,
+	)
+
 	if !config.HTTPStrm.Proxy {
 		jsonChain.Set(
 			bsePath+"SupportsDirectStream",
@@ -34,25 +41,25 @@ func processHTTPStrmPlaybackInfo(jsonChain *utils.JsonChain, bsePath string, ite
 		).Delete(
 			bsePath + "TrancodeLiveStartIndex",
 		)
-
-		var msgs []string
-		if directStreamURL != nil {
-			msgs = append(msgs, fmt.Sprintf("原直链播放链接: %s", *directStreamURL))
-			apikeypair, err := utils.ResolveEmbyAPIKVPairs(directStreamURL)
-			if err != nil {
-				logging.Warning("解析API键值对失败：", err)
-			}
-			directStreamURL := fmt.Sprintf("/Videos/%s/stream?MediaSourceId=%s&Static=true&%s", itemId, id, apikeypair)
-			jsonChain.Set(
-				bsePath+"DirectStreamUrl",
-				directStreamURL,
-			)
-			msgs = append(msgs, fmt.Sprintf("修改直链播放链接为: %s", directStreamURL))
-		}
-		logging.Infof("%s 强制禁止串流/转码行为，%s", id, strings.Join(msgs, ", "))
+		msgs = append(msgs, "禁止串流/转码行为")
 	} else {
-		logging.Infof("%s 保持原有串流/转码行为", id)
+		msgs = append(msgs, "保持原有串流/转码行为")
 	}
+
+	if directStreamURL != nil {
+		msgs = append(msgs, fmt.Sprintf("原直链播放链接: %s", *directStreamURL))
+		apikeypair, err := utils.ResolveEmbyAPIKVPairs(directStreamURL)
+		if err != nil {
+			logging.Warning("解析API键值对失败：", err)
+		}
+		directStreamURL := fmt.Sprintf("/Videos/%s/stream?MediaSourceId=%s&Static=true&%s", itemId, id, apikeypair)
+		jsonChain.Set(
+			bsePath+"DirectStreamUrl",
+			directStreamURL,
+		)
+		msgs = append(msgs, fmt.Sprintf("修改直链播放链接为: %s", directStreamURL))
+	}
+	logging.Infof("Media(id: %s) %s", id, strings.Join(msgs, ", "))
 }
 
 func processAlistStrmPlaybackInfo(jsonChain *utils.JsonChain, bsePath string, itemId string, id string, alistAddr string, directStreamURL *string, filepath string, size *int64) {
@@ -60,6 +67,20 @@ func processAlistStrmPlaybackInfo(jsonChain *utils.JsonChain, bsePath string, it
 	defer func() {
 		logging.Debugf("处理 AlistStrm %s PlaybackInfo 耗时：%s", id, time.Since(startTime))
 	}()
+
+	jsonChain.Set(
+		bsePath+"SupportsDirectPlay",
+		true,
+	)
+
+	var msgs []string
+
+	container := strings.TrimPrefix(path.Ext(filepath), ".")
+	jsonChain.Set(
+		bsePath+"Container",
+		container,
+	)
+	msgs = append(msgs, fmt.Sprintf("容器为： %s", container))
 
 	if !config.AlistStrm.Proxy {
 		jsonChain.Set(
@@ -77,50 +98,43 @@ func processAlistStrmPlaybackInfo(jsonChain *utils.JsonChain, bsePath string, it
 		).Delete(
 			bsePath + "TrancodeLiveStartIndex",
 		)
-
-		var msgs []string
-		if directStreamURL != nil {
-			msgs = append(msgs, fmt.Sprintf("原直链播放链接: %s", *directStreamURL))
-			url := fmt.Sprintf("/Videos/%s/stream?MediaSourceId=%s&Static=true", id, itemId)
-
-			apikeypair, err := utils.ResolveEmbyAPIKVPairs(&url)
-			if err != nil {
-				logging.Warning("解析API键值对失败：", err)
-			} else {
-				url += "&" + apikeypair
-			}
-			jsonChain.Set(
-				bsePath+"DirectStreamUrl",
-				url,
-			)
-			msgs = append(msgs, fmt.Sprintf("修改直链播放链接为: %s", url))
-		}
-		container := strings.TrimPrefix(path.Ext(filepath), ".")
-		jsonChain.Set(
-			bsePath+"Container",
-			container,
-		)
-		msgs = append(msgs, fmt.Sprintf("容器为： %s", container))
-		logging.Infof("%s 强制禁止串流/转码行为，%s", id, strings.Join(msgs, ", "))
+		msgs = append(msgs, "禁止串流/转码行为")
 	} else {
-		logging.Infof("%s 保持原有串流/转码行为", id)
+		msgs = append(msgs, "保持原有串流/转码行为")
+	}
+
+	if directStreamURL != nil {
+		msgs = append(msgs, fmt.Sprintf("原直链播放链接: %s", *directStreamURL))
+
+		apikeypair, err := utils.ResolveEmbyAPIKVPairs(directStreamURL)
+		if err != nil {
+			logging.Warning("解析API键值对失败：", err)
+		}
+		directStreamURL := fmt.Sprintf("/Videos/%s/stream?MediaSourceId=%s&Static=true&%s", itemId, id, apikeypair)
+		jsonChain.Set(
+			bsePath+"DirectStreamUrl",
+			directStreamURL,
+		)
+		msgs = append(msgs, fmt.Sprintf("修改直链播放链接为: %s", directStreamURL))
 	}
 
 	if size == nil {
 		alistClient, err := service.GetAlistClient(alistAddr)
 		if err != nil {
 			logging.Warning("获取 AlistClient 失败：", err)
-			return
+		} else {
+			fsGetData, err := alistClient.FsGet(&alist.FsGetRequest{Path: filepath, Page: 1})
+			if err != nil {
+				logging.Warning("请求 FsGet 失败：", err)
+			} else {
+				jsonChain.Set(
+					bsePath+"Size",
+					fsGetData.Size,
+				)
+				msgs = append(msgs, fmt.Sprintf("设置文件大小为： %d", fsGetData.Size))
+			}
 		}
-		fsGetData, err := alistClient.FsGet(&alist.FsGetRequest{Path: filepath, Page: 1})
-		if err != nil {
-			logging.Warning("请求 FsGet 失败：", err)
-			return
-		}
-		jsonChain.Set(
-			bsePath+"Size",
-			fsGetData.Size,
-		)
-		logging.Infof("%s 设置文件大小为：%d", id, fsGetData.Size)
 	}
+
+	logging.Infof("Media(id: %s) %s", id, strings.Join(msgs, ", "))
 }
